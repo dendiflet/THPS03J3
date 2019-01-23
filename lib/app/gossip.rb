74007f21a6b9ff7fc5d8fc456a0b9bd97372368e
dendiflet <bretin.iannis@gmail.com>
2019-01-23 19:53:59 +0100

    2.3.3 sur de sur, un array d'objets c'est trop beau

--- lib/app/gossip.rb
+++ lib/app/gossip.rb
@@ -26,20 +26,15 @@ class Gossip
 
 #binding.pry
 
-     # permet de créer un objet gossip
-    
     all_gossips << gossip_temp # permet de rajouter cet objet au array
   end
    
   print all_gossips # on renvoie le résultat
+
+  binding.pry
+
   end
 
 end
 
-  # création d'une array vide qui s'appelle all_gossips
-  # chaque ligne de ton CSV.each do |ligne|
-
-    # gossip_provisoire = Gossip.new(paramètres de la ligne) - permet de créer un objet gossip
-    # all_gossips << gossip_provisoire - permet de rajouter cet objet au array
-  # end
-  # return all_gossips - on renvoie le résultat
+  
